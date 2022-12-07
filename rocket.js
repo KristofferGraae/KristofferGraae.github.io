c4f1a2e1248f8fb036ce6dfb12d6ffef2d434fd7
KristofferGraae <114087171+KristofferGraae@users.noreply.github.com>
2022-12-07 10:46:39 -0500

    Add files via upload

---
+++ rocket.js
@@ -0,0 +1,16 @@
+class Plant {
+	constructor() {
+		this.size = 60;
+		this.x = width;
+		this.y = height - 80;
+	}
+	
+	move() {
+		this.x -= 5 ;
+	}
+	
+	show() {
+		
+		image(plantImg, this.x, this.y, this.size, this.size);
+	}
+}
